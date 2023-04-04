286efd27bea475ccea6e696835815f2c3172c7c5
Teo-Ingerman <116794946+Teo-Ingerman@users.noreply.github.com>
2023-04-04 10:50:39 +0200

    Add files via upload

---
+++ strictymode.js
@@ -0,0 +1,59 @@
+
+// uppgift 1
+
+const car = {
+    name:"toyota",
+    color:"white",
+    year:2022,
+}
+
+console.log(car[name], car[year])
+
+// uppgift 2
+
+//x är inte definierad
+"use strict"
+x = 5
+
+
+//uppgift 3
+
+
+function dividineds(numero_one, numero_two)
+{
+    "use strict"
+    return numero_one / numero_two
+}
+
+dividineds(3, 1)
+
+
+// uppgift 4
+
+const student = {
+    firstname:"hambus",
+    hand:"big",
+    foot:"small",
+}
+
+alert(student[firstname])
+
+
+// uppgift 5
+
+const persony = {
+    firstName : "Maryam",
+    lastName: "Rezaee",
+    country:"sweden",
+}
+
+
+// uppgift 6
+
+
+const person = {
+    stdname:"Hilda",
+    age:15,
+}
+
+alert(`${person[stdname]} är ${person[age]}`)
